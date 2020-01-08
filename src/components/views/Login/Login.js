f6f37828dc8828478104cc002a9af925268b87c4
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-01-08 09:31:49 +0100

    Add Waiter view using MUU and an attempt to prepare Login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import styles from './Login.scss';
+// import FormControl from '@material-ui/core/FormControl';
+// import Input from '@material-ui/core/Input';
+// import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+import styles from './Login.module.scss';
 
 const Login = () => {
   return (
     <div className={styles.component}>
-      <h2>Login view</h2>
+      <h2>Login</h2>
+      <form className={styles.form} autoComplete="off">
+        <TextField id="standard-basic" label="Standard" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </form>
     </div>
   );
 };

3cf01cf263935bba3b385a8a7179bbb0205d55ce
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-01-08 10:14:44 +0100

    Add Login view using MUI

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,20 +1,32 @@
 import React from 'react';
-// import FormControl from '@material-ui/core/FormControl';
-// import Input from '@material-ui/core/Input';
-// import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 import styles from './Login.module.scss';
 
 const Login = () => {
   return (
-    <div className={styles.component}>
-      <h2>Login</h2>
-      <form className={styles.form} autoComplete="off">
-        <TextField id="standard-basic" label="Standard" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <Container maxWidth='lg'>
+      <form className={styles.component} autoComplete="off">
+        <TextField
+          className={styles.textField}
+          required
+          id="login"
+          label="Login"
+        />
+        <TextField
+          className={styles.textField}
+          required
+          id="password"
+          label="Password"
+          type="password"
+        />
       </form>
-    </div>
+      <Button className={styles.button} variant="contained" color="primary" component={Link} to={`${process.env.PUBLIC_URL}/`}>
+        LOGIN
+      </Button>
+    </Container>
   );
 };
 

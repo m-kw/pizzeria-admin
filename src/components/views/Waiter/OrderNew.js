1c3a65fc38d054239904c771b1a3484eaf05edb5
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-01-10 12:04:53 +0100

    Add MUI styling to NewOrder view - add tableNo, order value, menu and order options

--- src/components/views/Waiter/OrderNew.js
+++ src/components/views/Waiter/OrderNew.js
@@ -1,11 +1,87 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import Button from '@material-ui/core/Button';
+import TableRow from '@material-ui/core/TableRow';
 import styles from './Waiter.module.scss';
+
+const menu = [
+  {id: 1, dish: 'pizza', options: ['pepper,', 'onion', 'olives']},
+  {id: 2, dish: 'coffee', options: ['lactose-free milk', 'small', 'big', 'no-milk']},
+  {id: 3, dish: 'salad', options: ['yellow cheese', 'goat cheese', 'tomato', 'cucumber', 'olives']},
+];
 
 const OrderNew = () => {
   return (
-    <div className={styles.component}>
-      <h2>Order new view</h2>
-    </div>
+    <Container>
+      <Paper className={styles.component}>
+        <h2>New order</h2>
+        <Grid container spacing={3}>
+          <Grid item xs={6} lg={3} container direction="column" justify="space-between" spacing={2}>
+            <Grid item>
+              <Paper>
+                <Typography variant="h5">Table No.</Typography>
+                <Divider />
+                <Button><Typography variant="h3">1</Typography></Button>
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper>
+                <Typography variant="h5">Value</Typography>
+                <Divider />
+                <Typography variant="h3">123$</Typography>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} lg={9} container direction="column" spacing={2}>
+            <Grid item>
+              <Paper>
+                <Typography variant="h5">Menu</Typography>
+                <Divider />
+                <Table>
+                  <TableBody>
+                    {menu.map(row => (
+                      <TableRow key={row.id} className={styles.tableRow}>
+                        <TableCell><Typography variant="h6">{row.dish}</Typography></TableCell>
+                        {row.options.map(option => (
+                          <TableCell key={option}>{option}</TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} lg={12} container direction="column" spacing={2}>
+            <Grid item>
+              <Paper>
+                <Typography variant="h5">Order</Typography>
+                <Divider />
+                <Table>
+                  <TableBody>
+                    {menu.map(row => (
+                      <TableRow key={row.id} className={styles.tableRow}>
+                        <TableCell><Typography variant="h6">{row.dish}</Typography></TableCell>
+                        {row.options.map(option => (
+                          <TableCell key={option}>{option}</TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 };
 
